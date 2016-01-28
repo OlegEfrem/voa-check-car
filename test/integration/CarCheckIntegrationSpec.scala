e339022fd669fab95460efe31abcfdad0bcd85e7
Oleg Efrem <oleg.efrem@yahoo.com>
2016-01-28 12:35:33 +0000

    Fix db not initialized in interation spec by passing the Application to it

--- test/integration/CarCheckIntegrationSpec.scala
+++ test/integration/CarCheckIntegrationSpec.scala
@@ -2,6 +2,7 @@ package integration
 
 import infrastructure.CarCheckSpec
 import org.scalatestplus.play.OneServerPerTest
+import play.api.Application
 import play.api.http.Status._
 import play.api.libs.ws._
 
@@ -11,18 +12,24 @@ class CarCheckIntegrationSpec extends CarCheckSpec with OneServerPerTest {
 
   "Checking car" should {
     "return existing car " in {
-      val result = WS.url(localhost + s"/check/car/$testRegNumber/$testMake?v5c=$testV5c").get().futureValue
+      val result = callApi(testRegNumber, testMake, Some(testV5c))
       result.status shouldBe OK
       val jsResult = result.json
       (jsResult \ "regNumber").as[String] shouldBe testRegNumber
       (jsResult \ "make").as[String] shouldBe testMake
-      (jsResult \ "v5c").as[String] shouldBe testV5c
+      (jsResult \ "v5c").as[Long] shouldBe testV5c
 
     }
 
     "return NOT FOUND for non existing car" in {
-      val result = WS.url(localhost + s"/check/car/someNumber/$testMake").get().futureValue
+      val result = callApi("someNubmer", testMake)
       result.status shouldBe NOT_FOUND
     }
   }
+
+  private def callApi(regNumber: String, make: String, v5c: Option[Long] = None)(implicit app: Application): WSResponse = {
+    val v5cPart = v5c.fold("")(num => s"?v5c=$num")
+    WS.url(localhost + s"/check/car/$regNumber/$make$v5cPart").get().futureValue
+  }
+
 }
